f56a3845bc3f3d161f9872c7e05a3022469fb562
Katie Duo Liu <katie_acting@live.ca>
2018-05-01 23:31:17 -0400

    Update missingLetters.js

--- missingLetters.js
+++ missingLetters.js
@@ -1,6 +1,5 @@
 //find the missing letter in the passed letter range and return it, if all letters are present return undefined
 function fearNotLetter(str) {
-  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var wrong = [];
 
   for (i = 0; i < str.length -1; i++) {
